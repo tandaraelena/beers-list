2ed10188f6e756b1a340b37df87a0bf9b580cd22
tandaraelena <tandaraelena@yahoo.com>
2019-06-26 19:02:29 +0100

    make the beer-details page to be responsive

--- src/components/beer-details/beer-details.jsx
+++ src/components/beer-details/beer-details.jsx
@@ -6,18 +6,22 @@ class BeerDetails extends React.Component {
   render() {
     const beer = JSON.parse(window.localStorage.getItem("beer"))
     return (
-      <div className="beer-details">
-        <Link className="back" to="/">
-          <span> {" < "} back</span>
-        </Link>
-        <h1 className="beer-name">{beer.name}</h1>
-        {/* <pre>
+      <div className="wrapper">
+        <div className="beer-details">
+          <Link className="back" to="/">
+            <span> {" < "} back</span>
+          </Link>
+          <h1 className="beer-name">{beer.name}</h1>
+          {/* <pre>
           {JSON.stringify(beer,null,2)}
         </pre> */}
-        <img src={beer.image_url}/>
-        <h2 style={{marginLeft:"20px"}}>ABV: {beer.abv}</h2>
-        <p className="tagline">{beer.tagline}</p>
-        <p>{beer.description}</p>
+          <img className="img" src={beer.image_url} />
+          <div className="beer-info">
+            <h2 className="abv">ABV: {beer.abv}</h2>
+            <p className="tagline">{beer.tagline}</p>
+            <p>{beer.description}</p>
+          </div>
+        </div>
       </div>
     )
   }
